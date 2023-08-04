dacd099937edac2b5f01505bfeab8deb154e3308
hemant878 <73839069+hemant878@users.noreply.github.com>
2023-08-04 16:10:11 +0530

    Add files via upload

---
+++ Bank/PassBook.js
@@ -0,0 +1,10 @@
+class PassBook{
+    constructor(date,balance,type,accountNumber){
+        this.date=date
+        this.balance=balance
+        this.type=type
+        this.accountNumber=accountNumber
+    }
+}
+
+module.exports=PassBook
